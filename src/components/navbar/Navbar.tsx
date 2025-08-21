99b28501662682cdcf7a64ab11591acc00819b1d
Bautista Heyde <bautistaheyde@hotmail.com>
2025-08-21 16:25:55 -0300

    info: cree la pagina de bulding para evitar el 404page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -37,10 +37,10 @@ const Navbar = () => {
   const navLinks = [
     { name: t.nosotros, href: '#Profile' },
     { name: t.profesionales, href: '#Profesionales' },
-    { name: t.clientes, href: '#Clientes' },
+    { name: t.clientes, href: '/building' },
     { name: t.areas, href: '#PracticeAreas' },
     { name: t.contenido, href: '#InstagramFeed' },
-    { name: t.contacto, href: '/contact' },
+    { name: t.contacto, href: '/building' },
   ];
 
   return (

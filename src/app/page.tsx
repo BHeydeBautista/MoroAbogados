721cc7b1d06686a91d813ee12f93f5b5ce9fb39c
Bautista Heyde <bautistaheyde@hotmail.com>
2025-11-17 13:58:26 -0300

    info: arregle visuales derechos societarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HeroCarousel from "@/components/hero/HeroCarousel";
 import PostsContent from "@/components/Posts/PostsContent";
-import PracticeAreas from "@/components/PracticeAreas";
+import PracticeAreas from "@/components/areas/PracticeAreas";
 import Profile from "@/components/Profile";
 
 export default function Home() {

83bcdca5d7ffa00829ea0c43be5f25a80ec778e5
Bautista Heyde <bautistaheyde@hotmail.com>
2025-12-08 12:17:21 -0300

    fix: deprecated functions

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
-    domains: ["res.cloudinary.com"],  
-  },
+  unoptimized: true,
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      pathname: "/**",
+    },
+  ],
+}
+
 };
 
 export default nextConfig;
